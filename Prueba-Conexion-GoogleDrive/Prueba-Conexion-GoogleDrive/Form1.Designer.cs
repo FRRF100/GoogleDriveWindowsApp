fdca7ecabb2e911197570072a3915f37af6b480a
felix rodriguez <felixrrf26@gmail.com>
2018-10-01 06:19:59 -0430

    Primera Subida de la app ya estando completamente funcional.

---
+++ Prueba-Conexion-GoogleDrive/Prueba-Conexion-GoogleDrive/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Prueba_Conexion_GoogleDrive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txtFileSelected = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.lblArchivos = new System.Windows.Forms.Label();
+            this.lblUploadPath = new System.Windows.Forms.Label();
+            this.txtUploadFolder = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblArchivosDrive = new System.Windows.Forms.Label();
+            this.txtDriveFiles = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(495, 474);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(135, 34);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Subir Archivos";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(12, 376);
+            this.txtEstado.Multiline = true;
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEstado.Size = new System.Drawing.Size(776, 81);
+            this.txtEstado.TabIndex = 0;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(12, 352);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.lblEstado.Size = new System.Drawing.Size(80, 21);
+            this.lblEstado.TabIndex = 1;
+            this.lblEstado.Text = "Estado:";
+            this.lblEstado.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtFileSelected
+            // 
+            this.txtFileSelected.Location = new System.Drawing.Point(12, 130);
+            this.txtFileSelected.Multiline = true;
+            this.txtFileSelected.Name = "txtFileSelected";
+            this.txtFileSelected.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtFileSelected.Size = new System.Drawing.Size(379, 219);
+            this.txtFileSelected.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Prueba_Conexion_GoogleDrive.Properties.Resources._1024px_Logo_of_Google_Drive_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblTitulo.Location = new System.Drawing.Point(84, 13);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(307, 70);
+            this.lblTitulo.TabIndex = 4;
+            this.lblTitulo.Text = "Aplicación para subir archivos a su cuenta de google drive.";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(12, 474);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(135, 34);
+            this.btnBrowser.TabIndex = 5;
+            this.btnBrowser.Text = "Abrir Explorador";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // lblArchivos
+            // 
+            this.lblArchivos.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblArchivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArchivos.Location = new System.Drawing.Point(12, 106);
+            this.lblArchivos.Name = "lblArchivos";
+            this.lblArchivos.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.lblArchivos.Size = new System.Drawing.Size(156, 21);
+            this.lblArchivos.TabIndex = 6;
+            this.lblArchivos.Text = "Archivos seleccionados:";
+            this.lblArchivos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblUploadPath
+            // 
+            this.lblUploadPath.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblUploadPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUploadPath.Location = new System.Drawing.Point(492, 38);
+            this.lblUploadPath.Name = "lblUploadPath";
+            this.lblUploadPath.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.lblUploadPath.Size = new System.Drawing.Size(119, 21);
+            this.lblUploadPath.TabIndex = 7;
+            this.lblUploadPath.Text = "Carpeta Destino:";
+            this.lblUploadPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtUploadFolder
+            // 
+            this.txtUploadFolder.Location = new System.Drawing.Point(617, 40);
+            this.txtUploadFolder.Name = "txtUploadFolder";
+            this.txtUploadFolder.Size = new System.Drawing.Size(171, 20);
+            this.txtUploadFolder.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // lblArchivosDrive
+            // 
+            this.lblArchivosDrive.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblArchivosDrive.Location = new System.Drawing.Point(419, 106);
+            this.lblArchivosDrive.Name = "lblArchivosDrive";
+            this.lblArchivosDrive.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.lblArchivosDrive.Size = new System.Drawing.Size(156, 21);
+            this.lblArchivosDrive.TabIndex = 9;
+            this.lblArchivosDrive.Text = "Archivos en Drive:";
+            this.lblArchivosDrive.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtDriveFiles
+            // 
+            this.txtDriveFiles.Location = new System.Drawing.Point(422, 130);
+            this.txtDriveFiles.Multiline = true;
+            this.txtDriveFiles.Name = "txtDriveFiles";
+            this.txtDriveFiles.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDriveFiles.Size = new System.Drawing.Size(366, 219);
+            this.txtDriveFiles.TabIndex = 10;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(653, 474);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(135, 34);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 520);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.txtDriveFiles);
+            this.Controls.Add(this.lblArchivosDrive);
+            this.Controls.Add(this.txtUploadFolder);
+            this.Controls.Add(this.lblUploadPath);
+            this.Controls.Add(this.lblArchivos);
+            this.Controls.Add(this.btnBrowser);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtFileSelected);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Servicio de Google Drive";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txtFileSelected;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Label lblArchivos;
+        private System.Windows.Forms.Label lblUploadPath;
+        private System.Windows.Forms.TextBox txtUploadFolder;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblArchivosDrive;
+        private System.Windows.Forms.TextBox txtDriveFiles;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Prueba-Conexion-GoogleDrive/Prueba-Conexion-GoogleDrive/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace Prueba_Conexion_GoogleDrive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            txtUploadFolder.Text = Environment.UserName;
+        }
+
+        static string[] Scopes = { DriveService.Scope.DriveFile }; //permiso otorgado: acceso limitado a archivos creados desde la app.
+        static string ApplicationName = "Prueba-Conexion-GoogleDrive";
+        string[] NameFiles;        
+        string UploadFolderID;
+        
+        private void ListFiles(DriveService service, ref string pageToken)
+        {
+            // Se definen los parámetros para la busqueda de elementos dentro de la carpeta parent.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 1;            
+            listRequest.Fields = "nextPageToken, files(name, id)";
+            listRequest.PageToken = pageToken;
+            listRequest.Q = "trashed=false";                       
+
+            //Se crea la lista de los archivos encontrados.
+            var request = listRequest.Execute();
+            if (request.Files != null && request.Files.Count > 0)
+            {
+                foreach (var file in request.Files)
+                {
+                    if(file.Name == Environment.UserName)//se verifica si ya existe la carpeta en drive.
+                    {
+                        UploadFolderID = file.Id;
+                        txtEstado.Text = UploadFolderID;
+                    }
+                    else
+                    {
+                        if(file.MimeType != "application/vnd.google-apps.folder")
+                        {
+                            txtDriveFiles.Text += string.Format("{0}", file.Name) + "\r\n";
+                        }                        
+                    }                    
+                }
+
+                pageToken = request.NextPageToken;
+                if (request.NextPageToken != null)
+                {
+                    Console.ReadLine();
+                }
+            }//
+            else
+            {
+                txtDriveFiles.Text += ("No se han encontrado archivos." + "\r\n");
+            }
+        }//        
+
+        //funcion para subir archivos a la carpeta destino.
+        private void UploadFile(string path, DriveService service, string folderUpload, string contentType)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File();
+            fileMetadata.Name = Path.GetFileName(path);            
+
+            fileMetadata.Parents = new List<string>
+            {
+                folderUpload
+            };
+
+            FilesResource.CreateMediaUpload request;
+            using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+            {                
+                request = service.Files.Create(fileMetadata, stream, contentType);
+                request.Fields = "id";
+                request.Upload();
+            }
+
+            var file = request.ResponseBody;            
+        }
+
+        //funcion para obtener la extension de los archivos a subir.
+        private static string GetMimeType(string fileName)
+        {
+            string mimeType = "application/unknown";
+            string ext = System.IO.Path.GetExtension(fileName).ToLower();
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+            return mimeType;
+        }
+
+        //se obtienes las credenciales de acceso a la cuenta de google drive a traves de un archivo.
+        private UserCredential GetCredentials()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, "driveApiCredentials", "drive-credentials.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    Environment.UserName,
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                //textBox1.Text = string.Format("Credential file saved to: " + credPath);
+            }
+            return credential;
+        }
+
+        //Se abre el explorador para seleccionar los archivos a subir.
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFileSelected.Text = "Archivos seleccionados para subir a la carpeta: => " + txtUploadFolder.Text + "\r\n\r\n";
+                NameFiles = openFileDialog1.FileNames; //se guardan los nombres de los archivos en un array.
+                foreach(string filename in openFileDialog1.FileNames)
+                {
+                    txtFileSelected.Text += filename + "\r\n";
+                }
+            }
+        }
+
+        //Funcion que inicia la subida de los archivos, comprobando las credenciales y procdiendo a crear la carpeta destino sino existe.
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            UserCredential credential;
+
+            credential = GetCredentials();
+
+            // Se inicia el servicio de autentificación de la api.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            service.HttpClient.Timeout = TimeSpan.FromMinutes(100);//se asigna el tiempo límite de las operaciones de subida de archivos.
+
+            //Se inician las busquedas de archivos y directorios en google drive.
+            string pageToken = null;
+            do
+            {
+                ListFiles(service, ref pageToken);                
+            } while (pageToken != null);
+
+            string folderid;
+
+            //Si la carpeta destino existe se mandan los archivos y sino se procede a mandar una request para crear la carpeta.
+            if (UploadFolderID != null)
+            {
+                folderid = UploadFolderID;                
+            }
+            else
+            {
+                //Se obtiene el id de la carpeta a través de su nombre.            
+                var fileMetadatas = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = txtUploadFolder.Text,
+                    MimeType = "application/vnd.google-apps.folder"
+                };                
+                var requests = service.Files.Create(fileMetadatas);
+                requests.Fields = "id";
+                var files = requests.Execute();                        
+                folderid = files.Id;
+                UploadFolderID = folderid;                
+            }            
+
+            //se construye el cuerpo de cada archivo a partir de su metadata para la request.
+            foreach (string filename in NameFiles)
+            {
+                Thread thread = new Thread(() =>
+                {
+                    UploadFile(filename, service, folderid, GetMimeType(filename));                    
+                });
+                thread.IsBackground = true;
+                thread.Start();
+            }         
+                    
+            txtEstado.Text += "\r\n" + "Archivos guardados exitosamente.";
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
